ee19061dbd981a2794b44e39cd3453a28fd66baa
Bui Phuong Thao <2411062533@hunre.edu.vn>
2026-08-19 21:32:06 +0700

    Lần commit đầu tiên

---
+++ bai32.py
@@ -0,0 +1,10 @@
+Ten = "Nguyen Van A"
+diem_toan = 8.5
+diem_van = 7.0
+so_luong_mon_hoc = 2
+MUC_LUONG_TOI_THIEU = 5000000
+print("Ten: ", Ten)
+print("Diem Toan: ", diem_toan)
+print("Diem Van: ", diem_van)
+print("So luong mon hoc: ", so_luong_mon_hoc)
+print("Muc luong toi thieu: ", MUC_LUONG_TOI_THIEU)
